b96750b6d82c9ca73fd8ee47c0b4b13ed1adcb25
Vignesh M <m.vigneshmlv347447@gmail.com>
2025-08-11 15:26:35 +0530

    Fix all SWC and build errors

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
     // Disable SWC compiler features
     removeConsole: false,
   },
+  // Force webpack to handle transpilation
+  webpack: (config, { dev, isServer }) => {
+    // Disable SWC loader
+    config.module.rules.forEach((rule) => {
+      if (rule.use && rule.use.loader === 'next-swc-loader') {
+        rule.use.loader = 'babel-loader'
+      }
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
