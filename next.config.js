8329a04f9fc194e466cbda67482e5d073503a6fd
Vignesh M <m.vigneshmlv347447@gmail.com>
2025-08-12 08:20:06 +0530

    Fix Next.js configuration errors

--- next.config.js
+++ next.config.js
@@ -1,45 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Completely disable SWC
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-    swcTraceProfiling: false,
-    runtime: undefined,
-  },
-  compiler: {
-    // Disable all SWC compiler features
-    removeConsole: false,
-    reactRemoveProperties: false,
-    relay: false,
-    emotion: false,
-    styledComponents: false,
-  },
-  // Force webpack to handle all transpilation
-  webpack: (config, { dev, isServer }) => {
-    // Remove any SWC loaders and replace with babel-loader
-    config.module.rules.forEach((rule) => {
-      if (rule.use) {
-        if (Array.isArray(rule.use)) {
-          rule.use.forEach((use) => {
-            if (use.loader && use.loader.includes('next-swc-loader')) {
-              use.loader = 'babel-loader'
-              use.options = {
-                presets: ['next/babel'],
-              }
-            }
-          })
-        } else if (rule.use.loader && rule.use.loader.includes('next-swc-loader')) {
-          rule.use.loader = 'babel-loader'
-          rule.use.options = {
-            presets: ['next/babel'],
-          }
-        }
-      }
-    })
-    
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
